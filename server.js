55ec2855a648aad115f42aa3fd175195a3a8601b
lucy-pill <schmidtiana95@gmail.com>
2022-11-01 19:26:17 +0900

    feat. resolver relation 추가

--- server.js
+++ server.js
@@ -1,36 +1,112 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-const typeDefs = gql`
+let tweets = [
+  {
+    id: '1',
+    text: 'first',
+    userId: '2',
+  },
+  {
+    id: '2',
+    text: 'second',
+    userId: '1',
+  },
+];
+
+let users = [
+  {
+    id: '1',
+    firstName: 'pill sang',
+    lastName: 'sung',
+  },
+  {
+    id: '2',
+    firstName: 'pill won',
+    lastName: 'sung',
+  },
+];
+
+const typeDefs = `#graphql
   type User {
     id: ID!
     username: String!
     firstName: String!
     lastName: String!
+    fullName: String!
   }
+  """
+  Tweet object represent a resource for a Tweet
+  """
   type Tweet {
     id: ID!
     text: String!
     author: User!
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
   }
   type Mutation {
-    postTweet(text: String!, userId: ID!): Tweet!
+    postTweet(text: String!, userId: ID!): Tweet
     deleteTweet(id: ID!): Boolean!
   }
 `;
 
-// const resolvers = {
-//   Query: {
-//     text: () => `text`,
-//     hello: () => `hello`,
-//   },
-// };
+const resolvers = {
+  Query: {
+    tweet(_, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+    allTweets() {
+      return tweets;
+    },
+    allUsers() {
+      return users;
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const user = users.find((user) => user.id === userId);
+      if (user) {
+        const newTweet = {
+          id: (parseInt(tweets.at(-1)['id']) + 1).toString(),
+          text,
+          userId,
+        };
+        tweets.push(newTweet);
+        return newTweet;
+      } else {
+        return { id: '', text: '', userId: '' };
+      }
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) {
+        return false;
+      } else {
+        tweets = tweets.filter((tweet) => tweet.id !== id);
+        return true;
+      }
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
+    },
+  },
+};
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
-  console.log(`Running on ${url}`);
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
 });
+
+console.log(`🚀  Server ready at: ${url}`);
